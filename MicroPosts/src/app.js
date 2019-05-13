cd61053aad85b022c2d3d7ab06a4f068325eb284
joycejoycejoyce <joycezhou757@gmail.com>
2019-05-13 19:57:10 -0400

    add ui

--- MicroPosts/src/app.js
+++
@@ -1,5 +0,0 @@
-//const person = require('./mymodule');
-
-import {person} from './mymodule2';
-
-console.log('the module info ', person);
--- MicroPosts/src/mymodule.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    name: "Joyce Zhou",
-    age: 23,
-}
