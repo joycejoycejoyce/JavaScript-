f6aaa3731ce413a957f3ffe39df2c87a640f60b1
joycejoycejoyce <joycezhou757@gmail.com>
2019-05-21 15:05:57 +0800

    show alert

--- BookList/index.js
+++ BookList/index.js
@@ -6,6 +6,7 @@ function Book(title, author, isbn){
 
 function UI(){
     this.selectors = {
+        container: '.container',
         list: '#book-form',
         listTitle: '#book-form-title',
         listAuthor : `#book-form-author`,
@@ -30,13 +31,31 @@ UI.prototype.clearFields = function(){
     document.querySelector(this.selectors.listISBN).value = ``;
 }
 
+UI.prototype.showAlert = function(msg, type){
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(msg));
+    div.className = `alert ${type}`;
+    console.log(`show alert function entered`);
+    const container = document.querySelector(this.selectors.container);
+    container.insertBefore(div, document.querySelector(this.selectors.list));
+    setTimeout(()=>{
+        document.querySelector('.alert').remove();
+    },3000)
+}
+
 document.getElementById('book-form').addEventListener('submit', submitForm);
 function submitForm(e){
     const title = document.getElementById('book-form-title').value;
     const author = document.getElementById('book-form-author').value;
     const isbn = document.getElementById('book-form-isbn').value;
-    const newBook = new Book(title, author, isbn);
     const ui = new UI();
+    if (title.trim() === '' || author.trim() === '' || isbn.trim()===''){
+        ui.showAlert('Please fill in all fields', 'error');
+        e.preventDefault();
+        return;
+    }
+    const newBook = new Book(title, author, isbn);
+    ui.showAlert('book added', 'success');
     ui.addBookToList(newBook);
     ui.clearFields();
     e.preventDefault();
