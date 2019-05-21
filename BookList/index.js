d6f036ec4b545353e16a8ac1c52ba0219d943c4b
joycejoycejoyce <joycezhou757@gmail.com>
2019-05-21 16:24:40 +0800

    delete book from list

--- BookList/index.js
+++ BookList/index.js
@@ -1,7 +1,28 @@
-function Book(title, author, isbn){
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
+function Book(){
+    this.key = 0;
+}
+
+Book.prototype.keyCount = function(performance){
+    if (performance === 'add'){
+        console.log(`keyCount add entereds`, this.key);
+        this.key +=1;
+        return;
+    }
+    this.key --;
+}
+
+Book.prototype.addBook = function(title, author, isbn){
+    const self = this;
+
+    const newBook = {
+        title:  title,
+        author:  author,
+        isbn:  isbn,
+        id: self.key,
+    }
+
+    self.keyCount('add');
+    return newBook;
 }
 
 function UI(){
@@ -14,15 +35,25 @@ function UI(){
         table: '#book-table',
     }
 }
+
+
 UI.prototype.addBookToList = function(book){
+    console.log(`book`, book);
     const table = document.querySelector(this.selectors.table);
     const newEl = document.createElement('tr');
+    newEl.id =`book-${book.id}`;
     newEl.innerHTML = `
         <th>${book.title}</th>
         <th>${book.author}</th>
         <th> ${book.isbn} </th>
+        <th class="trash">
+            <a class="trash-link" href="#">    
+                <i class="trash-icon fas fa-trash-alt"></i>
+            </a>
+        </th>
     `;
     table.appendChild(newEl);
+    
 }
 
 UI.prototype.clearFields = function(){
@@ -35,7 +66,6 @@ UI.prototype.showAlert = function(msg, type){
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(msg));
     div.className = `alert ${type}`;
-    console.log(`show alert function entered`);
     const container = document.querySelector(this.selectors.container);
     container.insertBefore(div, document.querySelector(this.selectors.list));
     setTimeout(()=>{
@@ -43,20 +73,43 @@ UI.prototype.showAlert = function(msg, type){
     },3000)
 }
 
+UI.prototype.deleteBookFromList = function(target){
+    let el;
+    el = target.tagName === "I" ?
+     target.parentElement.parentElement.parentElement 
+     : target.parentElement;
+     el.remove();
+    console.log(`book`, book.keyCount('minus'));
+}
+
 document.getElementById('book-form').addEventListener('submit', submitForm);
+document.getElementById('book-table').addEventListener('click', deleteBookFromList);
+
+const ui = new UI();
+const book = new Book();
+
 function submitForm(e){
     const title = document.getElementById('book-form-title').value;
     const author = document.getElementById('book-form-author').value;
-    const isbn = document.getElementById('book-form-isbn').value;
-    const ui = new UI();
+    const isbn = document.getElementById('book-form-isbn').value; 
     if (title.trim() === '' || author.trim() === '' || isbn.trim()===''){
         ui.showAlert('Please fill in all fields', 'error');
         e.preventDefault();
         return;
     }
-    const newBook = new Book(title, author, isbn);
+    const newBook = book.addBook(title, author, isbn);
     ui.showAlert('book added', 'success');
     ui.addBookToList(newBook);
     ui.clearFields();
     e.preventDefault();
 }
+
+
+function deleteBookFromList(e){
+    if (e.target.className.includes('trash')){
+        ui.deleteBookFromList(e.target);
+        ui.showAlert('Book removed', 'success');
+    }
+    e.preventDefault();
+}
+
